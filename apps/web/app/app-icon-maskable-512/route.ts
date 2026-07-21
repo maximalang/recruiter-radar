0b3149e52360ef96591580f6b5167d273ecac323
maximalang <maximal04040404@gmail.com>
2026-07-21 19:24:05 +0300

    fix(brand): restore exact-byte maskable icon route

---
+++ apps/web/app/app-icon-maskable-512/route.ts
@@ -0,0 +1,8 @@
+import { serveGeneratedAppIcon } from "../app-icon-file";
+
+export const dynamic = "force-static";
+export const revalidate = false;
+
+export function GET() {
+  return serveGeneratedAppIcon("maskable-512.png");
+}
