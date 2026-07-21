6284f2ffab7270f01628fab14f9fd367184742c8
maximalang <maximal04040404@gmail.com>
2026-07-21 20:46:15 +0300

    feat(brand): serve native 32px rounded favicon

---
+++ apps/web/app/tab-favicon-brand34-32/route.ts
@@ -0,0 +1,8 @@
+import { serveGeneratedTabFavicon } from "../tab-favicon-file";
+
+export const dynamic = "force-static";
+export const revalidate = false;
+
+export function GET() {
+  return serveGeneratedTabFavicon("tab-icon-32.png");
+}
