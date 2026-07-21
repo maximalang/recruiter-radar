4c0a3fbb8b28ca5259803a023d3558e3ba33051f
maximalang <maximal04040404@gmail.com>
2026-07-21 20:02:08 +0300

    fix(brand): serve rounded 192px tab fallback

--- apps/web/app/app-icon-192/route.ts
+++ apps/web/app/app-icon-192/route.ts
@@ -1,8 +1,8 @@
-import { serveGeneratedAppIcon } from "../app-icon-file";
+import { serveGeneratedTabIcon } from "../app-icon-file";
 
 export const dynamic = "force-static";
 export const revalidate = false;
 
 export function GET() {
-  return serveGeneratedAppIcon("app-icon-192.png");
+  return serveGeneratedTabIcon("tab-icon-192.png");
 }
