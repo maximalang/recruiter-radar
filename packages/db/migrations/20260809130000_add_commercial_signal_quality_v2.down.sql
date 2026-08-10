f005371c07f2b637397857c8a079d8337faeeab8
maximalang <maximal04040404@gmail.com>
2026-08-10 12:17:56 +0300

    db: reconcile Quality v2 migration sequence

---
+++ packages/db/migrations/20260809130000_add_commercial_signal_quality_v2.down.sql
@@ -0,0 +1,26 @@
+BEGIN;
+
+SET LOCAL lock_timeout = '5s';
+SET LOCAL statement_timeout = '5min';
+
+DO $$
+BEGIN
+  LOCK TABLE commercial_signal_quality_opportunity_lineage IN ACCESS EXCLUSIVE MODE;
+  LOCK TABLE commercial_signal_quality_evidence IN ACCESS EXCLUSIVE MODE;
+  LOCK TABLE commercial_signal_quality_snapshots IN ACCESS EXCLUSIVE MODE;
+
+  IF EXISTS (SELECT 1 FROM commercial_signal_quality_opportunity_lineage)
+     OR EXISTS (SELECT 1 FROM commercial_signal_quality_snapshots)
+     OR EXISTS (SELECT 1 FROM commercial_signal_quality_evidence) THEN
+    RAISE EXCEPTION
+      'commercial signal quality v2 rollback refused: append-only quality history exists';
+  END IF;
+END;
+$$;
+
+DROP TABLE commercial_signal_quality_opportunity_lineage;
+DROP FUNCTION validate_commercial_signal_quality_lineage();
+DROP TABLE commercial_signal_quality_evidence;
+DROP TABLE commercial_signal_quality_snapshots;
+
+COMMIT;
