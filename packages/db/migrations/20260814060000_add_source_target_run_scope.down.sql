048fd243bad05d38a0ecdb19cd9e084b10ee09bb
maximalang <maximal04040404@gmail.com>
2026-08-14 14:59:34 +0300

    refactor: simplify target-scope rollback

--- packages/db/migrations/20260814060000_add_source_target_run_scope.down.sql
+++ packages/db/migrations/20260814060000_add_source_target_run_scope.down.sql
@@ -2,19 +2,6 @@ BEGIN;
 SET LOCAL lock_timeout = '5s';
 SET LOCAL statement_timeout = '5min';
 
-DROP INDEX IF EXISTS source_scheduler_state_lease_idx;
-ALTER TABLE source_scheduler_state
-  DROP CONSTRAINT IF EXISTS source_scheduler_state_lease_contract,
-  DROP CONSTRAINT IF EXISTS source_scheduler_state_last_scheduler_outcome_check,
-  DROP COLUMN IF EXISTS lease_until,
-  DROP COLUMN IF EXISTS lease_owner;
-ALTER TABLE source_scheduler_state
-  ADD CONSTRAINT source_scheduler_state_last_scheduler_outcome_check CHECK (
-    last_scheduler_outcome IN (
-      'succeeded', 'failed', 'rate_limited', 'credential_gated'
-    )
-  );
-
 DROP INDEX IF EXISTS canonical_vacancy_publications_v1_target_idx;
 ALTER TABLE canonical_vacancy_publications_v1
   DROP COLUMN IF EXISTS source_target_key;
