b9acd88cf60f444e493343c2775a94184ab14652
maximalang <maximal04040404@gmail.com>
2026-08-14 19:18:32 +0300

    fix: reverse daily radar profile retry state

--- packages/db/migrations/20260814070000_add_daily_radar_run_lease.down.sql
+++ packages/db/migrations/20260814070000_add_daily_radar_run_lease.down.sql
@@ -2,6 +2,7 @@ BEGIN;
 SET LOCAL lock_timeout = '5s';
 SET LOCAL statement_timeout = '5min';
 
+DROP TABLE IF EXISTS daily_radar_profile_run_state;
 DROP TABLE IF EXISTS daily_radar_run_state;
 
 COMMIT;
