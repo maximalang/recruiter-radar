d3b372806c4b73676c610b607adc015f04451fe8
maximalang <maximal04040404@gmail.com>
2026-08-14 19:27:40 +0300

    fix: reverse daily radar fencing trigger

--- packages/db/migrations/20260814070000_add_daily_radar_run_lease.down.sql
+++ packages/db/migrations/20260814070000_add_daily_radar_run_lease.down.sql
@@ -2,6 +2,8 @@ BEGIN;
 SET LOCAL lock_timeout = '5s';
 SET LOCAL statement_timeout = '5min';
 
+DROP TRIGGER IF EXISTS daily_radar_run_state_fence_profiles ON daily_radar_run_state;
+DROP FUNCTION IF EXISTS fence_stale_daily_radar_profile_runs();
 DROP TABLE IF EXISTS daily_radar_profile_run_state;
 DROP TABLE IF EXISTS daily_radar_run_state;
 
